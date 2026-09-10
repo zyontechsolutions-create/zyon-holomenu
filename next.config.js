d730a52cabcb13ce0d66d11d254a79f1ee98b0da
zyontechsolutions-create <zyontechsolutions@gmail.com>
2026-09-10 12:26:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "*.supabase.co" }],
   },
 };
-module.exports = nextConfig;
+
+module.exports = withSentryConfig(nextConfig, {
+  org: "zyon-tech-solutions",
+  project: "zyon-holomenu",
+  silent: true,
+  widenClientFileUpload: true,
+});
